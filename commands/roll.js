008b798841c30d838b77973498c9016f84213568
kwpid <143108314+kwpid@users.noreply.github.com>
2025-05-11 15:28:43 -0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,34 +2,29 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: 'roll',
-    description: 'roll a dice',
+    description: 'rolls a dice with specified number of sides',
     execute(message, client) {
-        const args = message.content.slice(5).trim();
-        let sides = 6;
-        
-        if (args) {
-            const num = parseInt(args);
-            if (!isNaN(num) && num > 0 && num <= 100) {
-                sides = num;
-            } else {
-                const embed = new EmbedBuilder()
-                    .setColor('#292929')
-                    .setTitle("i cant count that high!")
-                    .setDescription("max is 100 sides")
-                    .setFooter({ text: 'patrick' })
-                    .setTimestamp();
-                
-                return message.reply({ embeds: [embed] });
-            }
-        }
-
-        const roll = Math.floor(Math.random() * sides) + 1;
         const embed = new EmbedBuilder()
             .setColor('#292929')
-            .setTitle("patrick's dice roll")
-            .setDescription(`i rolled a **${roll}** on a ${sides}-sided dice\n\n${reaction}`)
             .setFooter({ text: 'patrick' })
             .setTimestamp();
+
+        // Get the number of sides from the command
+        const args = message.content.split(' ');
+        const sides = parseInt(args[1]);
+
+        // Check if a valid number was provided
+        if (isNaN(sides) || sides < 1) {
+            embed.setTitle("error")
+                .setDescription("*i need a number to roll!*");
+            return message.reply({ embeds: [embed] });
+        }
+
+        // Generate random number between 1 and sides
+        const roll = Math.floor(Math.random() * sides) + 1;
+
+        embed.setTitle("dice roll")
+            .setDescription(`*i rolled a ${roll} on a ${sides}-sided dice!*`);
 
         message.reply({ embeds: [embed] });
     }
